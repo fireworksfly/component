d5688869f6a0924ed68c5c1de1ce3f7916a395d9
沈嘉徽 <247715089@qq.com>
2021-08-20 17:10:29 +0800

    final

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+  <MainTabBar>
+    <TabBarItem path="/home" active-color="deeppink">
+      <template v-slot:itemIcon>
+      <img src = "./assets/tabbar/home.svg" alt="">
+      </template>
+      <template v-slot:itemIconActive>
+        <img src = "./assets/tabbar/home_active.svg" alt="">
+      </template>
+      <template v-slot:itemText>
+      <div>首页</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="/category" active-color="deeppink">
+      <template v-slot:itemIcon>
+        <img  src = "./assets/tabbar/category.svg" alt="">
+      </template>
+      <template v-slot:itemIconActive>
+        <img  src = "./assets/tabbar/category_active.svg" alt="">
+      </template>
+      <template v-slot:itemText>
+        <div >分类</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="/shopcart" active-color="deeppink">
+      <template v-slot:itemIcon>
+        <img  src = "./assets/tabbar/shopcart.svg" alt="">
+      </template>
+      <template v-slot:itemIconActive>
+        <img  src = "./assets/tabbar/shopcart_active.svg" alt="">
+      </template>
+      <template v-slot:itemText>
+        <div>购物车</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="profile" active-color="deeppink">
+      <template v-slot:itemIcon>
+        <img src = "./assets/tabbar/profile.svg" alt="">
+      </template>
+      <template v-slot:itemIconActive>
+        <img src = "./assets/tabbar/profile_active.svg" alt="">
+      </template>
+      <template v-slot:itemText>
+        <div>我的</div>
+      </template>
+    </TabBarItem>
+  </MainTabBar>
+    <router-view></router-view>
   </div>
-  <router-view/>
 </template>
+<script>
+import MainTabBar from "./components/MainTabBar";
+import TabBarItem from "./components/TabBarItem";
+export default {
+  components: {MainTabBar, TabBarItem}
+}
 
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "assets/css/css.css";
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from "../view/home/Home";
+import CateGory from "../view/category/CateGory";
+import Profile from "../view/profile/Profile";
+import shopCart from "../view/shopcart/shopCart";
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: CateGory
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/shopcart',
+    component: shopCart
   }
 ]
 
